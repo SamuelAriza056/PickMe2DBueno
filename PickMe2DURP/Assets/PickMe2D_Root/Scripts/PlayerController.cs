d1bbbc1e1c018391d7e086e747824b2d31254b60
ainhowii <ainhoa.gz04@gmail.com>
2024-02-20 11:11:00 +0100

    Movimiento y salto del minero

--- PickMe2DURP/Assets/PickMe2D_Root/Scripts/PlayerController.cs
+++ PickMe2DURP/Assets/PickMe2D_Root/Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Variables de referencia
+    private Rigidbody2D playerRB;
+    private Animator anim;
+    private float horizontalInput;
+
+    //Variables de estadisticas del player
+    public float speed;
+    public float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        playerRB.velocity = new Vector2(horizontalInput * speed, playerRB.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
